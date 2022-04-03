321fc0474aca8bca79501f4678a97e19f98059a7
PedroPereira78 <you@example.com>
2022-04-04 00:34:33 +0100

    Teste registo

--- Shop-Cart/src/main/java/controller/Controller.java
+++ Shop-Cart/src/main/java/controller/Controller.java
@@ -7,18 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
+	JavaBeans registo = new JavaBeans();
+	
 	public Controller() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -38,7 +38,13 @@ public class Controller extends HttpServlet {
 		if (action.equals("/main")) {
 			registos(request, response);
 
+		} else if (action.equals("/insert")) {
+			novoRegisto(request, response);
+		}else {
+			response.sendRedirect("/index.html");
 		}
+		
+		
 		// Teste Conexao
 		/* dao.testeConexao(); */
 	}
@@ -47,6 +53,15 @@ public class Controller extends HttpServlet {
 	protected void registos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("registo.jsp");
+
+	}
+	//Novo registo
+	protected void novoRegisto(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//teste dados formulario
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("telemovel"));
+		System.out.println(request.getParameter("email"));
 
 	}
 }
